c588a2a238fb7065a71cc9c9f4c8d9b3c6ca486a
aman <amanv4775@gmail.com>
2023-07-22 23:23:51 +0530

    first

--- src/App.js
+++ src/App.js
@@ -1,25 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{useState, useEffect} from "react";
+import Navbar from "./Components/Navbar";
+import Book from "./Components/Book";
+import axios from "axios";
+import "./styles.css"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+const App=()=>{
+    const [books, setBooks] = useState([]); 
+    useEffect(() => {
+        async function fetchBooksByQuery(query) {
+          try {
+            const response = await axios.get(
+              "https://www.googleapis.com/books/v1/volumes",
+              {
+                params: {
+                  q: query,
+                },
+              }
+            );
+            return response.data.items;
+          } catch (error) {
+            console.log(error);
+            return [];
+          }
+        }
+    
+        async function fetchData() {
+          const query1 = "Sherlock Holmes";
+          const query2 = "harry potter";
+    
+          const booksQuery1 = await fetchBooksByQuery(query1);
+          const booksQuery2 = await fetchBooksByQuery(query2);
+    
+          const allBooks = [...booksQuery1, ...booksQuery2];
+    
+          setBooks(allBooks);
+        }
+    
+        fetchData();
+      }, []);
+
+    return (
+        <div>
+            <Navbar setBooks={setBooks}/>
+            <Book books={books}/>
+        </div>
+    )
 }
 
 export default App;
